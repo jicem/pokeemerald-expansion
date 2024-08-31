47263fb5a6ca2087d4aea4bba3c7427d5d55e2f5
jicem <jmanigo@email.sc.edu>
2024-08-31 18:24:14 -0400

    dialogue changes, item changes, and level caps

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -7640,7 +7640,7 @@ static void Cmd_yesnoboxstoplearningmove(void)
         {
             PlaySE(SE_SELECT);
 
-            if (gBattleCommunication[1] != 0)
+            if (gBattleCommunication[1] == 0)
                 gBattlescriptCurrInstr = cmd->noInstr;
             else
                 gBattlescriptCurrInstr = cmd->nextInstr;
@@ -7650,7 +7650,7 @@ static void Cmd_yesnoboxstoplearningmove(void)
         else if (JOY_NEW(B_BUTTON))
         {
             PlaySE(SE_SELECT);
-            gBattlescriptCurrInstr = cmd->noInstr;
+            gBattlescriptCurrInstr = cmd->nextInstr;
             HandleBattleWindow(YESNOBOX_X_Y, WINDOW_CLEAR);
         }
         break;
